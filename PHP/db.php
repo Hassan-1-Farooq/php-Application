42d2719f3acb573962be7161e36d66e2ac23b25c
Hassan-1-Farooq <153942278+Hassan-1-Farooq@users.noreply.github.com>
2024-09-20 21:18:32 +0100

    Update db.php
    Update DB connection for deployment

--- PHP/db.php
+++ PHP/db.php
@@ -1,8 +1,22 @@
 <?php
-$localhost="localhost";
-$username="root";
-$password="";
-$db ="techwave";
+// $localhost="localhost";
+// $username="root";
+// $password="";
+// $db ="techwave";
+// $conn = mysqli_connect($localhost,$username,$password,$db);
+
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+//   }
+//   else
+//   {
+//     echo "";
+//   }
+
+$localhost="sql200.infinityfree.com";
+$username="if0_37348350";
+$password="CountryStyle12";
+$db ="if0_37348350_techwaves";
 $conn = mysqli_connect($localhost,$username,$password,$db);
 
 if (!$conn) {
@@ -12,4 +26,4 @@ if (!$conn) {
   {
     echo "";
   }
-  ?>
+  
